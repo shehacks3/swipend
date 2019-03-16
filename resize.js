91074404bc163a05ddfea53b225d10ad489bd49b
Anemmeabasi Bassey <anemmeabasi1@gmail.com>
2019-03-16 17:16:19 -0400

    Add files via upload

---
+++ resize.js
@@ -0,0 +1,8 @@
+setTimeout(function(){
+  document.getElementById('sizer').style.height = '56px';
+}, 20);
+
+setTimeout(function(){
+  document.getElementById('sizer').style.width = '91px';
+}, 230); // the lower this number is, the more likely a bad render
+
